cd9b8f9c92fdb687d8035da62646459d0ab180a5
HarikrishnanKM <harikrishnankm2010@outlook.com>
2023-06-15 08:10:16 +0000

    n1

--- network.tf
+++ network.tf
@@ -3,8 +3,3 @@ module "network" {
   version = "3.5.0"
   resource_group_name = "hari-workshop"
 }
-
-credentials "app.terraform.io" {
-  # valid user API token
-  token = "bo8Zok7QOlo7sg.atlasv1.dtHhw2aHnuTXwKoAkuTUYW229Nm0xbRJywDhbXQjfScsIuo73fE6g4sDBR80pXRarzI"
-}
